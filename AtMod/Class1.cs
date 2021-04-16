f0596d96f2a4900e745fd87fc2dc6a57dc9e8d3b
Abhinay Agarwal <abhinayagarwal@live.com>
2021-04-16 15:24:52 -0500

    Added assets

---
+++ AtMod/Class1.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using RoR2;
+
+namespace Atti1a
+{
+    [BepInDependency("com.bepis.r2api")]
+    //Change these
+    [BepInPlugin("com.Atti1a.AtMod", "Atti1a's Mod", "1.0.0")]
+    public class AtMod : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Logger.LogMessage("Loaded AtMod!");
+            On.EntityStates.Huntress.ArrowRain.OnEnter += (orig, self) =>
+            {
+                // [The code we want to run]
+                Chat.AddMessage("You used Huntress's Arrow Rain!");
+
+                // Call the original function (orig)
+                // on the object it's normally called on (self)
+                orig(self);
+            };
+        }
+    }
+}
